8b724072a10946c0dc405e0cc8bb469887c6dea8
Kiledjia <56278781+Kiledjia@users.noreply.github.com>
2021-03-02 17:15:42 -0800

    Create view-log.page.ts

---
+++ sleeptracker/src/app/view-log/view-log.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-view-log',
+  templateUrl: './view-log.page.html',
+  styleUrls: ['./view-log.page.scss'],
+})
+export class ViewLogPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
