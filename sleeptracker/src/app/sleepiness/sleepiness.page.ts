5d98a5082aba7e624e1cfac52a0aaf973b098096
18cpham3 <phamcathy6575@gmail.com>
2021-03-03 21:25:22 -0800

    Update sleepiness.page.ts

--- sleeptracker/src/app/sleepiness/sleepiness.page.ts
+++ sleeptracker/src/app/sleepiness/sleepiness.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { StanfordSleepinessData } from '../data/stanford-sleepiness-data';
+import { SleepService } from '../services/sleep.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-sleepiness',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sleepiness.page.scss'],
 })
 export class SleepinessPage implements OnInit {
+  StanfordSleepinessValue:number;
+  sleeptime:string;
 
-  constructor() { }
+  constructor(public sleepService:SleepService, public alertController:AlertController) { }
 
   ngOnInit() {
   }
 
+  getSleepinessValue(event){
+    this.StanfordSleepinessValue = event.target.value;
+  }
+
+  onSubmit(){
+
+    if (this.StanfordSleepinessValue != null && this.sleeptime != null)  {
+      let sleepinessValue = new StanfordSleepinessData(this.StanfordSleepinessValue);
+      this.sleepService.logSleepinessData(sleepinessValue);
+      this.presentSuccessAlert()
+    }
+    else{
+      this.presentNullAlert()
+    }
+  }
+
+  async presentNullAlert() {
+   const alert = await this.alertController.create({
+     cssClass: 'nullAlert',
+     header: 'Incomplete Submission',
+     message: 'Please fill out your sleepiness value before submitting.',
+     buttons: ['OK']
+   });
+
+   await alert.present();
+  }
+
+  async presentSuccessAlert() {
+  const alert = await this.alertController.create({
+    cssClass: 'successAlert',
+    header: 'Saved!',
+    message: 'Your sleepiness value was logged.',
+    buttons: ['OK']
+  });
+
+   await alert.present();
+  }
 }
